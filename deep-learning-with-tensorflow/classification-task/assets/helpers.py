6926ab154723bfebcf49b5af0d8bad76053a1a9e
bfusion <barbara.fusinska@gmail.com>
2017-10-03 14:33:33 +0100

    Added padding when plotting decision boundary in the helpers.py code of the Deep Learning with TensorFlow scenario

--- deep-learning-with-tensorflow/classification-task/assets/helpers.py
+++ deep-learning-with-tensorflow/classification-task/assets/helpers.py
@@ -20,9 +20,9 @@ def read_and_visualise_data(file_name='classification_data.csv', out_file='data.
     return df
 
 def plot_decision_boundary(model, X, labels, out_file='decision_boundary.png'):
-    # Set min and max values
-    x_min, x_max = X[:, 0].min(), X[:, 0].max()
-    y_min, y_max = X[:, 1].min(), X[:, 1].max()
+    # Set min and max values and add a little padding
+    x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+    y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
     h = 0.01
     # Generate a grid of points with distance h between them
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
