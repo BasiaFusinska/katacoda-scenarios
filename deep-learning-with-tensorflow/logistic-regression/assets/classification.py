d10f2b692a55ad785ed1396678688a6278afb3bb
bfusion <barbara.fusinska@gmail.com>
2017-10-03 16:00:07 +0100

    Added code files to the Logistic Regression scenario

---
+++ deep-learning-with-tensorflow/logistic-regression/assets/classification.py
@@ -0,0 +1,44 @@
+import helper as h
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# Task 1: Read and visualise the data
+data = # TODO: use read_data or read_and_visualise_data from helper
+print data
+
+# Task 2: Split the data into train and test with 70:30 and 50:50 ratios
+train70, test30 = # TODO: use train_test_split from sklearn.model_selection
+train50, test50 =
+
+# Task 3: Create and train three models
+model70 = # TODO: use LogisticRegressionCV from sklearn.linear_model and fit the model
+model50 =
+model100 =
+
+# Task 4: Predict the values using trained models
+pred_train70 = # TODO: Use predict function
+pred_test70 =
+pred_train50 =
+pred_test50 =
+pred_train100 =
+
+# Accuracy calculations
+accuracy_train70 = accuracy_score(train70['label'], pred_train70) * 100
+accuracy_test50 = accuracy_score(test30['label'], pred_test70) * 100
+accuracy_train70 = accuracy_score(train50['label'], pred_train50) * 100
+accuracy_test50 = accuracy_score(test50['label'], pred_test50) * 100
+accuracy_train100 = accuracy_score(data['label'], pred_train100) * 100
+
+print 'Train accuracy 70-30: %d %%' % accuracy_train70
+print 'Test accuracy 70-30: %d %%' % accuracy_test70
+print 'Train accuracy 50-50: %d %%' % accuracy_train50
+print 'Test accuracy 50-50: %d %%' % accuracy_test50
+print 'Train accuracy 100-0: %d %%' % accuracy_train100
+
+# Plot decision boundary
+h.plot_decision_boundary(lambda x: model70.predict(x), data[['x1', 'x2']].values, data['label'], out_file='decision_boundary70.png')
+h.plot_decision_boundary(lambda x: model50.predict(x), data[['x1', 'x2']].values, data['label'], out_file='decision_boundary50.png')
+h.plot_decision_boundary(lambda x: model100.predict(x), data[['x1', 'x2']].values, data['label'])
---
+++ deep-learning-with-tensorflow/logistic-regression/assets/helper.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os.path
+
+def read_data(file_name='non-linear.csv'):
+    df = pd.read_csv(file_name)
+    return df
+
+def visualise_data(X1, X2, labels, out_file):
+    plt.scatter(X1, X2, c=labels, s=40, cmap=plt.cm.Spectral);
+    plt.savefig(out_file)
+
+def read_and_visualise_data(file_name='non-linear.csv', out_file='data.png'):
+    df = read_data(file_name)
+    if not os.path.isfile(out_file):
+         visualise_data(df['x1'], df['x2'], df['label'], out_file)
+    return df
+
+def plot_decision_boundary(model, X, labels, out_file='decision_boundary.png'):
+    # Set min and max values and add a little padding
+    x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+    y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[:, 0], X[:, 1], c=labels, cmap=plt.cm.Spectral)
+    plt.savefig(out_file)
